320b8a8b0c3c57ca95203efe63cf5956efb22397
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-12-28 13:01:40 +0600

    Call exportReport from controller

--- jasper-report/src/main/java/com/roman/jasperreport/controller/ProductsController.java
+++ jasper-report/src/main/java/com/roman/jasperreport/controller/ProductsController.java
@@ -1,10 +1,13 @@
 package com.roman.jasperreport.controller;
 
 import com.roman.jasperreport.model.Products;
+import com.roman.jasperreport.service.ReportService;
 import com.roman.jasperreport.serviceImpl.ProductsServiceImpl;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileNotFoundException;
 import java.util.List;
 
 @RestController
@@ -14,6 +17,9 @@ public class ProductsController {
     @Autowired
     private ProductsServiceImpl productsServiceimpl;
 
+    @Autowired
+    private ReportService reportService;
+
     @PostMapping("/add")
     public void addProduct(@RequestBody Products p){
         productsServiceimpl.addProduct(p);
@@ -22,6 +28,11 @@ public class ProductsController {
     @GetMapping("/all")
     public List<Products> productsList(){
         return productsServiceimpl.getAllProducts();
+    }
+
+    @GetMapping("/report/{format}")
+    public String createReport(@PathVariable String format) throws JRException, FileNotFoundException {
+        return reportService.exportReport(format);
     }
 
 }
